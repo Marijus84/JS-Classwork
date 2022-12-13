1d55f568d3ba3127574057be652ec03e8f981213
“Marijus84” <marijus.romanovas@hotmail.com>
2022-12-13 09:35:05 +0200

    add functions tasks

--- 5_functions_and_events/script.js
+++ 5_functions_and_events/script.js
@@ -94,3 +94,34 @@ sum(2, 3);
 const sum2 = (a) => a * a;
 
 sum2(2);
+
+//? Task 1
+//function declaration
+function printHometown() {
+  console.log("Telšiai");
+}
+printHometown();
+
+// arrow function
+const printHometown2 = () => console.log("Telšiai");
+printHometown2();
+
+//? Task 2
+//function declaration
+function min(a, b) {
+  if (a < b) {
+    return a;
+  }
+  return b;
+}
+console.log(min(1, 5));
+
+//arrow function
+const min2 = (a, b) => {
+  return Math.min(a, b);
+};
+
+console.log(min2(1, 5));
+
+const number = Math.ceil(Math.random() * 23);
+console.log(number);
